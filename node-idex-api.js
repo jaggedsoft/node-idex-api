eb3db5652a731d4900907a4e49d60fa71fdde2bc
Jon Eyrick <social@nextlocal.net>
2018-11-28 04:48:47 +0100

    add payload fields & returnOrderBook count

--- node-idex-api.js
+++ node-idex-api.js
@@ -1,9 +1,9 @@
 module.exports = function () {
     'use strict';
-    const WebSocket = require('ws');
-    const axios = require('axios');
+    const WebSocket = require( 'ws' );
+    const axios = require( 'axios' );
 
-    async function api(action, json = {}) {
+    async function api( action, json = {} ) {
         const userAgent = 'Mozilla/4.0 (compatible; Node IDEX API)';
         const contentType = 'application/json';
         let headers = {
@@ -11,107 +11,111 @@ module.exports = function () {
             'Content-type': contentType
         };
         try {
-            const response = await axios.request({
+            const response = await axios.request( {
                 url: action,
                 headers: headers,
                 method: 'POST',
                 baseURL: 'https://api.idex.market/'
-            }, json);
-            if ( response && response.status !== 200 ) return new Error(JSON.stringify(response.data));
+            }, json );
+            if ( response && response.status !== 200 ) return new Error( JSON.stringify( response.data ) );
             return response.data;
-        } catch (error) {
-            return new Error(JSON.stringify(error.response.data));
+        } catch ( error ) {
+            return new Error( JSON.stringify( error.response.data ) );
         }
-    };
+    }
 
     return {
-        returnTicker: async function returnTicker(ticker) {
+        returnTicker: async function returnTicker( ticker ) {
             const json = { market: `${ticker}` }
-            return await api(`returnTicker`, json)
+            return await api( `returnTicker`, json )
         },
-        returnTickers: async function returnTickers(ticker) {
+        returnTickers: async function returnTickers( ticker ) {
             return await returnTicker()
         },
         return24Volume: async function return24Volume() {
-            return await api(`return24Volume`)
+            return await api( `return24Volume` )
         },
-        returnOpenOrders: async function returnOpenOrders(market, address = null) {
-            console.log({ market })
-            return await api(`returnOpenOrders`, { market, address })
+        returnOpenOrders: async function returnOpenOrders( market, address = null ) {
+            console.log( { market } )
+            return await api( `returnOpenOrders`, { market, address } )
         },
-        returnOrderBook: async function returnOrderBook(market) {
-            return await api(`returnOrderBook`, { market })
+        returnOrderBook: async function returnOrderBook( market, count = 100 ) {
+            return await api( `returnOrderBook`, { market, count } )
         },
         returnTradeHistory: async function returnTradeHistory() {
-            return await api(`returnTradeHistory`)
+            return await api( `returnTradeHistory` )
         },
         returnCurrencies: async function returnCurrencies() {
-            return await api(`returnCurrencies`)
+            return await api( `returnCurrencies` )
         },
         returnBalances: async function returnBalances() {
-            return await api(`returnBalances`)
+            return await api( `returnBalances` )
         },
         returnCompleteBalances: async function returnCompleteBalances() {
-            return await api(`returnCompleteBalances`)
+            return await api( `returnCompleteBalances` )
         },
         returnDepositsWithdrawals: async function returnDepositsWithdrawals() {
-            return await api(`returnDepositsWithdrawals`)
+            return await api( `returnDepositsWithdrawals` )
         },
         returnOrderTrades: async function returnOrderTrades() {
-            return await api(`returnOrderTrades`)
+            return await api( `returnOrderTrades` )
         },
-        returnNextNonce: async function returnNextNonce() {
-            return await api(`returnNextNonce`)
+        returnNextNonce: async function returnNextNonce( address ) {
+            return await api( `returnNextNonce`, { address } )
         },
         returnContractAddress: async function returnContractAddress() {
-            return await api(`returnContractAddress`)
+            return await api( `returnContractAddress` )
         },
-        order: async function order() {
-            return await api(`order`)
+        // Create limit order
+        order: async function order( payload ) {
+            return await api( `order`, payload )
         },
-        trade: async function trade() {
-            return await api(`trade`)
+        // Fill an existing trade or trades
+        trade: async function trade( payload ) {
+            return await api( `trade`, payload )
         },
-        cancel: async function cancel() {
-            return await api(`cancel`)
+        // Cancel an order
+        cancel: async function cancel( payload ) {
+            return await api( `cancel`, payload )
         },
-        withdraw: async function withdraw() {
-            return await api(`withdraw`)
+        // Withdraw funds
+        withdraw: async function withdraw( payload ) {
+            return await api( `withdraw`, payload )
         },
-        
+
         // WebSocket live ticker updates
-        subscribe: function subscribe(ticker) {
-            const socket = new WebSocket('wss://api-cluster.idex.market');
-            socket.on('message', message => console.log(message));
+        subscribe: function subscribe( ticker ) {
+            const socket = new WebSocket( 'wss://api-cluster.idex.market' );
+            socket.on( 'message', message => console.log( message ) );
 
             //ws.on('pong', handleSocketHeartbeat); // Update timer & reconnect zombie sockets
 
-            socket.on('error', error => {
-                console.error('WebSocket error: ', error);
+            socket.on( 'error', error => {
+                console.error( 'WebSocket error: ', error );
                 socket.close();
-            });
+            } );
 
-            socket.on('open', () => {
-                setInterval(() => socket.ping(), 10000);
-                socket.send(JSON.stringify({ subscribe: ticker }), error => {
+            socket.on( 'open', () => {
+                setInterval( () => socket.ping(), 10000 );
+                socket.send( JSON.stringify( { subscribe: ticker } ), error => {
                     if ( error ) {
-                        console.error('WebSocket send error: ', error);
+                        console.error( 'WebSocket send error: ', error );
                         socket.close();
                     }
-                });
-            });
+                } );
+            } );
         },
-      
+
         // Convert to sortable array. {"ETHBTC":{}} to [{symbol:"ETHBTC"}]
         obj_to_array: json => {
-          let output = [];
-          for ( let key in json ) {
-            let obj = json[key];
-            obj.symbol = key;
-            output.push(obj);
-          }
-          return output;
+            let output = [];
+            for ( let key in json ) {
+                let obj = json[key];
+                obj.symbol = key;
+                output.push( obj );
+            }
+            return output;
         }
-        
+
     }
 }();
